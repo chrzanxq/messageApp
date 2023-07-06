cd64331ae7db4102953907fa18ac53be77581f98
chrzanxq <marcinch199@gmail.com>
2023-07-06 21:27:35 +0200

    Login View start

---
+++ whatsapp/src/components/InputComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+  <label class="block text-sm font-medium loading-6 text-gray-900" >
+   
+  </label>
+  <div class="mt-2">
+    <input
+      
+      class="
+        form-input
+        block
+        w-full
+        rounded-md
+        border-0
+        py-1.5
+        text-gray-900
+        shadow-sm
+        ring-1
+        ring-inset
+        ring-gray-300
+        placeholder:text-gray-400
+        focus:ring-2'
+        focus:ring-inset
+        focus:ring-sky-600
+        sm:text-dash-sm
+        sm:leading-6
+       
+      "
+    />
+  </div>
+</div>
+</template>
---
+++ whatsapp/src/views/LoginView.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div class="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form class="space-y-6" >
+          <InputComponent
+            
+            id="name"
+            label="Name"
+            
+          />
+          <InputComponent
+            id="email"
+            label="Email address"
+            type="email"
+           
+          />
+          <InputComponent
+            id="password"
+            label="Password"
+            type="password"
+           
+          />
+          <div>
+            <Button  :fullWidth="true" type="submit">
+             
+            </Button>
+          </div>
+        </form>
+  
+        <div class="mt-6">
+          <div class="relative">
+            <div class="absolute inset-0 flex items-center">
+              <div class="w-full border-t border-gray-300"></div>
+            </div>
+            <div class="relative flex justify-center text-sm">
+              <span class="bg-white px-2 text-gray-500">Or continue with</span>
+            </div>
+          </div>
+  
+          <div class="mt-6 flex gap-2">
+            
+          </div>
+        </div>
+  
+        <div class="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+           
+          </div>
+          <div class="underline cursor-pointer">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+
+<script setup>
+import InputComponent from '../components/InputComponent.vue';
+</script>
